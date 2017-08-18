8424d624d363f050cd0aa957de4694de221ab0c5
Varvara <varvara.malysheva@saritasa.com>
2017-08-18 11:52:41 +0700

    before pull

--- db/database.py
+++ db/database.py
@@ -1,4 +1,18 @@
 class Cart(object):
+    def __init__(self):
+        self.total = 0
+        self.items = {}
+        self.db = []
 
-    def add(self, product, quantity):
-        pass
+    def add_item(self, item_name, quantity):
+
+        #self.items.clear()
+        #if item_name not in self.items.keys():
+        self.items = {"parent": item_name, "quantity": quantity, "type": 'sku'}
+        self.db.append(self.items)
+
+    def delete_item(self, item):
+        for it in self.db:
+            if it.get("parent") == item:
+                self.db.remove(it)
+                return "item delete"
--- rpc/payments.py
+++ rpc/payments.py
@@ -7,8 +7,9 @@ class Payments(object):
     name = 'PaymentsRPC'
     stripe.api_key = "sk_test_K5QUkUgvUNKvDD9fEGYBI6Gi"
 
+    @rpc
     def add_in_cart(self):
-        
+        return "Okey"
 
     def transaction(self):
         order = stripe.Order.create()
